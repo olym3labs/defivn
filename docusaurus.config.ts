f672778a327564619e7fe7f8cc11cc68090d8035
ZxStim <thang.victor.luong@gmail.com>
2024-01-04 00:13:23 +0700

    changed onBrokenLinks to ignore

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   organizationName: 'defivn', // Usually your GitHub org/user name.
   projectName: 'defivn', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
